884da0b5cdae7b2dacc3a85f78efaef061ccadca
Maksimple Dev Studio <polyaev-maks@ya.ru>
2020-05-09 10:29:43 +0300

    interface

--- importpalmira.php
+++ importpalmira.php
@@ -28,6 +28,8 @@ if (!defined('_PS_VERSION_')) {
     exit;
 }
 
+require_once _PS_MODULE_DIR_ . 'importpalmira/vendor/autoload.php';
+
 use MaximCode\ImportPalmira\ImportForm as ImportForm;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\VarDumper\VarDumper;
