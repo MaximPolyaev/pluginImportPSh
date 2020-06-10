78f608a502821e5619e21fc61eb8cd70259dcfa9
Maksimple Dev Studio <polyaev-maks@ya.ru>
2020-06-11 00:11:43 +0300

    ImportDB image

--- src/ImportDB.php
+++ src/ImportDB.php
@@ -8,6 +8,10 @@ use PrestaShop\PrestaShop\Adapter\Entity\Category;
 use PrestaShop\PrestaShop\Adapter\Entity\Context;
 use PrestaShop\PrestaShop\Adapter\Entity\Feature;
 use PrestaShop\PrestaShop\Adapter\Entity\FeatureValue;
+use PrestaShop\PrestaShop\Adapter\Entity\Image;
+use PrestaShop\PrestaShop\Adapter\Entity\ImageManager;
+use PrestaShop\PrestaShop\Adapter\Entity\ImageType;
+use PrestaShop\PrestaShop\Adapter\Entity\Language;
 use PrestaShop\PrestaShop\Adapter\Entity\Manufacturer;
 use PrestaShop\PrestaShop\Adapter\Entity\Product;
 use PrestaShop\PrestaShop\Adapter\Entity\ProductDownload;
@@ -255,8 +259,50 @@ class ImportDB
             }
 
             if (isset($data_item['delete_existing_images'])) {
-                if ($data_item['delete_existing_images'] === '1') {
+                if ($data_item['delete_existing_images'] === '1' || true) {
                     $product->deleteImages();
+                }
+            }
+
+            if (isset($data_item['image'])) {
+                VarDumper::dump(Shop::getContext());
+                $images_url = explode(',', $data_item['image']);
+                $images_url = array_map(function($image_url) {
+                    return trim($image_url);
+                }, $images_url);
+
+                $product_has_images = (bool) Image::getImages($this->language_id, (int) $product->id);
+                foreach ($images_url as $key => $url) {
+                    $url = trim($url);
+                    $error = false;
+                    if (!empty($url)) {
+                        $url = str_replace(' ', '%20', $url);
+
+                        $image = new Image();
+                        $image->id_product = (int) $product->id;
+                        $image->position = Image::getHighestPosition($product->id) + 1;
+                        $image->cover = (!$key && !$product_has_images) ? true : false;
+                        $alt = 'test alt';
+                        if (strlen($alt) > 0) {
+                            $image->legend = self::createMultiLangField($alt);
+                        }
+                        // file_exists doesn't work with HTTP protocol
+                        if (($field_error = $image->validateFields(false, true)) === true &&
+                            ($lang_field_error = $image->validateFieldsLang(false, true)) === true && $image->add()) {
+                            // associate image to selected shops
+//                            $image->associateTo($shops);
+                            if (!self::copyImg($product->id, $image->id, $url, 'products', true)) {
+                                $image->delete();
+                                VarDumper::dump("Error copying image: $url");
+                            }
+                        } else {
+                            $error = true;
+                        }
+                    } else {
+                        $error = true;
+                    }
+
+                    if ($error) VarDumper::dump('error import img');
                 }
             }
         }
@@ -560,5 +606,148 @@ class ImportDB
         $this->context->shop->id = $save_shop_id;
         Shop::setContext($save_shop_context);
         return $products;
+    }
+
+    public static function createMultiLangField($field)
+    {
+        $res = [];
+        foreach (Language::getIDs(false) as $id_lang) {
+            $res[$id_lang] = $field;
+        }
+
+        return $res;
+    }
+
+    protected static function copyImg($id_entity, $id_image = null, $url = '', $entity = 'products', $regenerate = true)
+    {
+        $tmpfile = tempnam(_PS_TMP_IMG_DIR_, 'ps_import');
+        $watermark_types = explode(',', \Configuration::get('WATERMARK_TYPES'));
+
+        switch ($entity) {
+            default:
+            case 'products':
+                $image_obj = new Image($id_image);
+                $path = $image_obj->getPathForCreation();
+
+                break;
+            case 'categories':
+                $path = _PS_CAT_IMG_DIR_ . (int) $id_entity;
+
+                break;
+            case 'manufacturers':
+                $path = _PS_MANU_IMG_DIR_ . (int) $id_entity;
+
+                break;
+            case 'suppliers':
+                $path = _PS_SUPP_IMG_DIR_ . (int) $id_entity;
+
+                break;
+            case 'stores':
+                $path = _PS_STORE_IMG_DIR_ . (int) $id_entity;
+
+                break;
+        }
+
+        $url = urldecode(trim($url));
+        $parced_url = parse_url($url);
+
+        if (isset($parced_url['path'])) {
+            $uri = ltrim($parced_url['path'], '/');
+            $parts = explode('/', $uri);
+            foreach ($parts as &$part) {
+                $part = rawurlencode($part);
+            }
+            unset($part);
+            $parced_url['path'] = '/' . implode('/', $parts);
+        }
+
+        if (isset($parced_url['query'])) {
+            $query_parts = array();
+            parse_str($parced_url['query'], $query_parts);
+            $parced_url['query'] = http_build_query($query_parts);
+        }
+
+        if (!function_exists('http_build_url')) {
+            require_once _PS_TOOL_DIR_ . 'http_build_url/http_build_url.php';
+        }
+
+        $url = http_build_url('', $parced_url);
+
+        $orig_tmpfile = $tmpfile;
+
+        if (\Tools::copy($url, $tmpfile)) {
+            // Evaluate the memory required to resize the image: if it's too much, you can't resize it.
+            if (!ImageManager::checkImageMemoryLimit($tmpfile)) {
+                @unlink($tmpfile);
+
+                return false;
+            }
+
+            $tgt_width = $tgt_height = 0;
+            $src_width = $src_height = 0;
+            $error = 0;
+            ImageManager::resize($tmpfile, $path . '.jpg', null, null, 'jpg', false, $error, $tgt_width, $tgt_height, 5, $src_width, $src_height);
+            $images_types = ImageType::getImagesTypes($entity, true);
+
+            if ($regenerate) {
+                $previous_path = null;
+                $path_infos = array();
+                $path_infos[] = array($tgt_width, $tgt_height, $path . '.jpg');
+                foreach ($images_types as $image_type) {
+                    $tmpfile = self::get_best_path($image_type['width'], $image_type['height'], $path_infos);
+
+                    if (ImageManager::resize(
+                        $tmpfile,
+                        $path . '-' . stripslashes($image_type['name']) . '.jpg',
+                        $image_type['width'],
+                        $image_type['height'],
+                        'jpg',
+                        false,
+                        $error,
+                        $tgt_width,
+                        $tgt_height,
+                        5,
+                        $src_width,
+                        $src_height
+                    )) {
+                        // the last image should not be added in the candidate list if it's bigger than the original image
+                        if ($tgt_width <= $src_width && $tgt_height <= $src_height) {
+                            $path_infos[] = array($tgt_width, $tgt_height, $path . '-' . stripslashes($image_type['name']) . '.jpg');
+                        }
+                        if ($entity == 'products') {
+                            if (is_file(_PS_TMP_IMG_DIR_ . 'product_mini_' . (int) $id_entity . '.jpg')) {
+                                unlink(_PS_TMP_IMG_DIR_ . 'product_mini_' . (int) $id_entity . '.jpg');
+                            }
+                            if (is_file(_PS_TMP_IMG_DIR_ . 'product_mini_' . (int) $id_entity . '_' . (int) Context::getContext()->shop->id . '.jpg')) {
+                                unlink(_PS_TMP_IMG_DIR_ . 'product_mini_' . (int) $id_entity . '_' . (int) Context::getContext()->shop->id . '.jpg');
+                            }
+                        }
+                    }
+                    if (in_array($image_type['id_image_type'], $watermark_types)) {
+                        \Hook::exec('actionWatermark', array('id_image' => $id_image, 'id_product' => $id_entity));
+                    }
+                }
+            }
+        } else {
+            @unlink($orig_tmpfile);
+
+            return false;
+        }
+        unlink($orig_tmpfile);
+
+        return true;
+    }
+
+    protected static function get_best_path($tgt_width, $tgt_height, $path_infos)
+    {
+        $path_infos = array_reverse($path_infos);
+        $path = '';
+        foreach ($path_infos as $path_info) {
+            list($width, $height, $path) = $path_info;
+            if ($width >= $tgt_width && $height >= $tgt_height) {
+                return $path;
+            }
+        }
+        return $path;
     }
 }
